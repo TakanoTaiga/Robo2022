4725d64de94e1dd98afeda8761765264a33d8a2d
TakanoTaiga <ttttghghnb554z@outlook.jp>
2022-07-05 23:54:45 +0900

    Add GCText & setupTasks

--- RoboNetwork2.py
+++ RoboNetwork2.py
@@ -2,13 +2,56 @@ import socket
 import netifaces
 import argparse #Command line option
 
-class UDPHandler():
+class setupTasks():
     def parser(self):
         parser = argparse.ArgumentParser() # Create object
         parser.add_argument("--nwiface", help="Please write your network interface" , default="lo0" , type=str)
         args = parser.parse_args()
         return args
 
+class GCText():
+    GCInfoMsgs = {
+        "leftJoystic" : ['J' , [1.0 , 0.0]] ,
+        "rightJoystic" : ['J' ,[0.0 , 0.0]] ,
+        "leftTrigger" : ['T' , 0.0] ,
+        "rightTrigger" : ['T' , 0.0] ,
+        "dpadLeft" : ['B' , False] ,
+        "dpadUp" : ['B' , False] ,
+        "dpadRight" : ['B' , False] ,
+        "dpadDown" : ['B' , False] ,
+        "buttonX" : ['B' , False] ,
+        "buttonY" : ['B' , False] ,
+        "buttonB" : ['B' , False] ,
+        "buttonA" : ['B' , False] ,
+        "leftThumbstickButton" : ['B' , False] ,
+        "rightThumbstickButton" : ['B' , False] ,
+        "optionButton" : ['B' , False] ,
+        "menuButton" : ['B' , False] ,
+        "leftShoulderButton" : ['B' , False] ,
+        "rightShoulderButton" : ['B' , False] ,
+        "leftTriggerButton" : ['B' , False] ,
+        "rightTriggerButton" : ['B' , False]
+    }
+
+    def NWtoGCmsg(self , NWMessage):
+        lists = NWMessage.rsplit(",")
+        del lists[0]
+        del lists[len(lists) - 1]
+        for text in lists:
+            info = text.rsplit(":")
+            GCInfoMsg = self.GCInfoMsgs[info[0]]
+            if GCInfoMsg[0] == 'J':
+                self.GCInfoMsgs[info[0]][1][0] = float(info[1])
+                self.GCInfoMsgs[info[0]][1][1] = float(info[2])
+            elif GCInfoMsg[0] == 'T':
+                self.GCInfoMsgs[info[0]][1] = float(info[1])
+            elif GCInfoMsg[0] == 'B':
+                if info[1] == 'false':
+                    self.GCInfoMsgs[info[0]][1] = False
+                elif info[1] == 'true':
+                    self.GCInfoMsgs[info[0]][1] = True
+
+class UDPHandler(GCText , setupTasks):
     def recv(self):
         while True:
             data , addr = self.UDPServerSocket.recvfrom(self.Buffsize)
@@ -30,7 +73,7 @@ class UDPHandler():
             pos = stringData.find('WHATISNODEIP')
             if pos != -1 :
                 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-                sendData = str("MYNODEIP" + self.address)
+                sendData = str("MYNODEIP" + self.address[0])
                 sock.sendto(sendData.encode('utf-8'), (ip , 64201))
                 print(sendData , (ip , 64201))
                 sock.close()
@@ -54,15 +97,18 @@ class UDPHandler():
                 sock.close()
 
             pos = -1
+            pos = stringData.find('GCINFO')
+            if pos != -1 :
+                GCText.NWtoGCmsg(self, NWMessage=stringData)
+                print(GCText.GCInfoMsgs)
+
+            pos = -1
             pos = stringData.find('CLOSESESSION')
             if pos != -1 :
-                self.close()                
-
-    def close(self):
-        self.UDPServerSocket.close()
+                self.UDPServerSocket.close()                     
 
     def __init__(self):
-        args = self.parser()
+        args = setupTasks.parser(self)
         self.address = (netifaces.ifaddresses(args.nwiface)[netifaces.AF_INET][0]['addr'] , 64201)
         self.Buffsize = 1024
         self.UDPServerSocket = socket.socket(socket.AF_INET , socket.SOCK_DGRAM)
