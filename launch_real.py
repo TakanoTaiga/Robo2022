6abcd8cc2069954fd4fd2d884ab17de0d22f63ef
Taiga <53041471+TakanoTaiga@users.noreply.github.com>
2022-08-11 20:38:44 +0900

    Add files via upload

---
+++ launch_real.py
@@ -0,0 +1,60 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='robo2022',
+            executable='vel2motor',
+            parameters=[{'motor_position':'V1'}],
+            remappings=[
+                ('robo2022/motor_power' , 'robo2022/motor_power/V1')
+            ]
+        ),
+        Node(
+            package='robo2022',
+            executable='vel2motor',
+            parameters=[{'motor_position':'V2'}],
+            remappings=[
+                ('robo2022/motor_power' , 'robo2022/motor_power/V2')
+            ]
+        ),
+        Node(
+            package='robo2022',
+            executable='vel2motor',
+            parameters=[{'motor_position':'V3'}],
+            remappings=[
+                ('robo2022/motor_power' , 'robo2022/motor_power/V3')
+            ]
+        ),
+        Node(
+            package='robo2022',
+            executable='vel2motor',
+            parameters=[{'motor_position':'V4'}],
+            remappings=[
+                ('robo2022/motor_power' , 'robo2022/motor_power/V4')
+            ]
+        ),
+        Node(
+            package='robo2022',
+            executable='joy2vel',
+            remappings=[
+                ('/joy' , '/sc_client/joy')
+            ]
+        ),
+        Node(
+            package='robo2022',
+            executable='physicalMDC'
+        ),
+        Node(
+            package='robo2022',
+            executable='serial',
+            parameters=[{'device_file':'/dev/ttyACM0'}]
+        ),
+        Node(
+            package='sc_client',
+            executable='server',
+            parameters=[{'port' :64201},{'nic' : 'wlp3s0'}]
+        )
+
+    ])
