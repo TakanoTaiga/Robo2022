39d96cd5740b3445445ab45cdf5d4a4a31c81afc
Taiga Takano <ttttghghnb554z@outlook.jp>
2022-09-06 00:56:46 +0900

    launch_real update
    Signed-off-by: Taiga Takano <ttttghghnb554z@outlook.jp>

--- launch_real.py
+++ launch_real.py
@@ -1,67 +1,40 @@
-from launch import LaunchDescription
+from launch import LaunchDescription , actions
 from launch_ros.actions import Node
+
 
 def generate_launch_description():
     return LaunchDescription([
         Node(
             package='robo2022',
-            executable='vel2motor',
-            parameters=[{'motor_position':'V1'}],
-            remappings=[
-                ('robo2022/motor_power' , 'robo2022/motor_power/V1')
-            ]
-        ),
-        Node(
-            package='robo2022',
-            executable='vel2motor',
-            parameters=[{'motor_position':'V2'}],
-            remappings=[
-                ('robo2022/motor_power' , 'robo2022/motor_power/V2')
-            ]
-        ),
-        Node(
-            package='robo2022',
-            executable='vel2motor',
-            parameters=[{'motor_position':'V3'}],
-            remappings=[
-                ('robo2022/motor_power' , 'robo2022/motor_power/V3')
-            ]
-        ),
-        Node(
-            package='robo2022',
-            executable='vel2motor',
-            parameters=[{'motor_position':'V4'}],
-            remappings=[
-                ('robo2022/motor_power' , 'robo2022/motor_power/V4')
-            ]
-        ),
-        Node(
-            package='robo2022',
             executable='joy2vel',
             remappings=[
-                ('/joy' , '/sc_client/joy')
-            ]
-        ),
-        Node(
-            package='robo2022',
-            executable='physicalMDC'
-        ),
-        Node(
-            package='robo2022',
-            executable='serial',
-            parameters=[{'device_file':'/dev/ttyACM0'}]
+                ('/joy' , '/sc_client/joy'),
+                ('/SmartUI' , '/sc_client/SmartUI')
+            ],
+            on_exit=actions.Shutdown(),
         ),
         Node(
             package='sc_client',
             executable='server',
-            parameters=[{'port' :64201},{'nic' : 'wlp3s0'}]
+            parameters=[{'nic' : 'wlp3s0'} , {'c1' : 'Up'} , {'c2' : 'Down'} , {'s1' : 'Return Power'} , {'s2' : 'Shot Power'} , {'debug' : False}],
+            on_exit=actions.Shutdown(),
         ),
         Node(
             package='joy',
             executable='joy_node',
             remappings=[
                 ('/joy' , '/sc_client/joy')
-            ]
+            ],
         ),
-
+        Node(
+            package='robo2022',
+            executable='robo2022',
+            on_exit=actions.Shutdown(),
+        ),
+        Node(
+            package='robo2022',
+            executable='mdc2022Connect',
+            parameters=[{'debug' : False}],
+            on_exit=actions.Shutdown(),
+        )
     ])
